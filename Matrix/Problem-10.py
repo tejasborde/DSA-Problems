9fe8cab61af8aaf512701d0c88cd673027cb65e0
Tejas Borde <tejas.borde@kpit.com>
2022-06-05 19:31:08 +0530

    Few Problems Solved

---
+++ Matrix/Problem-10.py
@@ -0,0 +1,34 @@
+import sys
+sys.stdout = open('E:/Computer Engineering/SE COMP 2020/Data Structures/output.txt', 'w')
+sys.stdin = open('E:/Computer Engineering/SE COMP 2020/Data Structures/input.txt', 'r')
+
+# A Program to prints common element
+# in all rows of matrix
+
+M = 4
+N = 5
+
+def printCommonElements(mat):
+
+	mp = dict()
+	for j in range(N):
+		mp[mat[0][j]] = 1
+
+	for i in range(1, M):
+		for j in range(N):
+			if (mat[i][j] in mp.keys() and
+							mp[mat[i][j]] == i):
+								
+				mp[mat[i][j]] = i + 1
+
+				# If this is last row
+				if i == M - 1:
+					print(mat[i][j], end = " ")
+
+mat = [[1, 2, 1, 4, 8],
+	[3, 7, 8, 5, 1],
+	[8, 7, 7, 3, 1],
+	[8, 1, 2, 7, 9]]
+
+printCommonElements(mat)
+
---
+++ Matrix/Problem-9.py
@@ -0,0 +1,52 @@
+import sys
+sys.stdout = open('E:/Computer Engineering/SE COMP 2020/Data Structures/output.txt', 'w')
+sys.stdin = open('E:/Computer Engineering/SE COMP 2020/Data Structures/input.txt', 'r')
+
+
+
+
+
+mat= [[10, 20, 30, 40], 
+    [15, 25, 35, 45], 
+    [24, 29, 37, 48], 
+    [32, 33, 39, 50]]
+r=len(mat)
+c=len(mat[0])
+k=7
+
+
+
+
+def countNumbersLessThanNumber(row,num):
+    low=0
+    high=len(row)-1
+
+    while(low<=high):
+        mid=(low+high)//2
+
+        if(row[mid]<=num):
+            low=mid+1
+        else:
+            high=mid-1
+    return low
+
+def KthElementinSortedArray(mat,r,c,k):
+    low=1
+    high=10**9
+
+    while(low<=high):
+        mid=(low+high)//2
+    
+        NumbersLessThanNumber=0
+        for i in range(r):
+            NumbersLessThanNumber+=countNumbersLessThanNumber(mat[i],mid)
+
+        if(NumbersLessThanNumber<k):
+            low=mid+1
+        else:
+            high=mid-1
+    return low
+
+
+
+print(KthElementinSortedArray(mat, r, c, k))
